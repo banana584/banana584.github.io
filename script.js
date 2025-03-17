95dd815969d7a7faeccb92f802f3f6d8d1d3116d
banana584 <alexander.david.watts@gmail.com>
2025-03-17 16:52:50 +0000

    Update script.js
    Added functionality to the button in index.html

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const title = document.getElementById("title");
+const button = document.getElementById("button");
 
+function change_text() {
+  title.innerHTML = "Button clicked!";
+}
+
+button.addEventListener("click", change_text);
