01b21e47e884cf026940eeb9a8e398a34162ef0f
banana584 <alexander.david.watts@gmail.com>
2025-03-17 16:59:59 +0000

    Update script.js
    Added more functionality to button to change title color to green when button is clicked

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const button = document.getElementById("button");
 
 function change_text() {
   title.innerHTML = "Button clicked!";
+  title.style.color = "green";
 }
 
 button.addEventListener("click", change_text);
