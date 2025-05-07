6ef0260959d545fed9c77a0eb55bf691eee33185
banana584 <alexander.david.watts@gmail.com>
2025-05-07 17:37:14 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const title = document.getElementById("title");
-const button = document.getElementById("button");
-
-function change_text() {
-  title.innerHTML = "Button clicked!";
-  title.style.color = "green";
-}
-
-button.addEventListener("click", change_text);
